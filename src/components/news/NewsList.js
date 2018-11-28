ca60b099111804e48ffdc8b4929544d76346953f
BryanNilsen <bryannilsen@gmail.com>
2018-11-28 14:28:21 -0600

    navbar, login, application views functioning

---
+++ src/components/news/NewsList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import "./News.css"
+
+export default class NewsList extends Component {
+    render () {
+        return (
+            <section className="news list_title">
+            {
+
+                <h1>Hello News!!</h1>
+
+            }
+            </section>
+        )
+    }
+}
+
