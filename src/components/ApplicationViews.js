a51a9f0a180679a02d60c1086109ab67d4809334
BryanNilsen <bryannilsen@gmail.com>
2018-11-29 10:57:34 -0600

    deleted commented code

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -25,64 +25,6 @@ render() {
           return <Redirect to="/login" />
         }
       }} />
-      {/* <Route exact path="/animals" render={(props) => {
-            if (this.isAuthenticated()) {
-              return <AnimalList {...props}
-                animals={this.state.animals}
-                owners={this.state.owners}
-                animalOwners={this.state.animalOwners}
-                deleteAnimal={this.deleteAnimal}
-              />
-            } else {
-              return <Redirect to="/login" />
-            }
-          }} />
-          <Route exact path="/employees" render={(props) => {
-            if (this.isAuthenticated()) {
-              return <EmployeeList
-                deleteEmployee={this.deleteEmployee}
-                animals={this.state.animals}
-                employees={this.state.employees} />
-            } else {
-              return <Redirect to="/login" />
-            }
-          }} />
-
-          <Route exact path="/owners" render={(props) => {
-            if (this.isAuthenticated()) {
-              return <OwnerList owners={this.state.owners} deleteOwner={this.deleteOwner} />
-            } else {
-              return <Redirect to="/login" />
-            }
-          }} />
-          <Route exact path="/search" render={(props) => {
-            console.log("props: ", props)
-            return <Search results={this.props.searchResults} />
-          }} />
-          <Route path="/animals/:animalId(\d+)" render={(props) => {
-            return <AnimalDetail {...props}
-              deleteAnimal={this.deleteAnimal}
-              animals={this.state.animals}
-              employees={this.state.employees}
-              owners={this.state.owners}
-              animalOwners={this.state.animalOwners}
-            />
-          }} />
-          <Route path="/employees/:employeeId(\d+)" render={(props) => {
-            return <EmployeeDetail {...props} deleteEmployee={this.deleteEmployee} employees={this.state.employees} />
-          }} />
-          <Route path="/owners/:ownerId(\d+)" render={(props) => {
-            return <OwnerDetail {...props} deleteOwner={this.deleteOwner} owners={this.state.owners} />
-          }} />
-          <Route path="/locations/:locationId(\d+)" render={(props) => {
-            return <LocationDetail {...props} deleteLocation={this.deleteLocation} locations={this.state.locations} />
-          }} />
-
-          <Route path="/animals/new" render={(props) => {
-            return <AnimalForm {...props}
-              addAnimal={this.addAnimal}
-              employees={this.state.employees} />
-          }} />*/}
       <Route path="/login" component={Login} />
 
     </React.Fragment>
