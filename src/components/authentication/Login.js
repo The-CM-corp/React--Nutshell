a1f8de61fec3df81a2e82119fe75dfd51f45cee2
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2018-11-29 15:55:11 -0600

    built register form, checks for existing user

--- src/components/authentication/Login.js
+++ src/components/authentication/Login.js
@@ -5,9 +5,12 @@ export default class Login extends Component {
 
   // Set initial state
   state = {
-    email: "",
-    password: "",
+    loginEmail: "",
+    loginPassword: "",
     remember: "",
+    registerEmail: "",
+    registerPassword: "",
+    registerName: "",
     users: []
   }
 
@@ -26,53 +29,104 @@ export default class Login extends Component {
     this.setState(stateToChange)
   }
 
-  // Simplistic handler for login submit
+  // Handle for Login (existing user)
   handleLogin = (e) => {
     e.preventDefault()
     this.state.users.forEach(user => {
-      if (user.email === this.state.email) {
+      if (user.email === this.state.loginEmail) {
         if (this.state.remember === "") {
           sessionStorage.setItem(
             "userId", user.id
-          )} else {
-            localStorage.setItem(
-              "userId", user.id
-            )
-          }
+          )
+        } else {
+          localStorage.setItem(
+            "userId", user.id
+          )
+        }
+      }
+    })
+  }
+
+  // Handle register for new user
+  handleRegister = (e) => {
+    e.preventDefault()
+    this.state.users.forEach(user => {
+      if(user.email === this.state.registerEmail) {
+        alert("This email is already taken")
       }
     })
   }
 
 
-render() {
-  return (
-    <form className="bryans__class" onSubmit={this.handleLogin}>
-      <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-      <label htmlFor="inputEmail">
-        Email address
+  render() {
+    return (
+      <React.Fragment>
+        <form className="bryans__class" onSubmit={this.handleLogin}>
+          <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+          <label htmlFor="inputEmail">
+            Email address
                 </label>
-      <br />
-      <input onChange={this.handleFieldChange} type="email"
-        id="email"
-        placeholder="Email address"
-        required="" autoFocus="" />
-      <br />
-      <label htmlFor="inputPassword">
-        Password
+          <br />
+          <input onChange={this.handleFieldChange} type="email"
+            id="loginEmail"
+            placeholder="Email address"
+            required="" autoFocus="" />
+          <br />
+          <label htmlFor="inputPassword">
+            Password
                 </label>
-      <br />
-      <input onChange={this.handleFieldChange} type="password"
-        id="password"
-        placeholder="Password"
-        required="" />
-      <br />
-      Remember me <input onChange={this.handleFieldChange} type="checkbox"
-        id="remember" />
-      <br />
-      <button type="submit">
-        Sign in
+          <br />
+          <input onChange={this.handleFieldChange} type="password"
+            id="loginPassword"
+            placeholder="Password"
+            required="" />
+          <br />
+          Remember me <input onChange={this.handleFieldChange} type="checkbox"
+            id="remember" />
+          <br />
+          <button type="submit">
+            Sign in
                 </button>
-    </form>
-  )
-}
+        </form>
+
+
+        <form className="bryans__class" onSubmit={this.handleRegister}>
+          <h1 className="h3 mb-3 font-weight-normal">New here? Create an account</h1>
+
+          <label htmlFor="inputName">
+            Name
+                </label>
+          <br />
+          <input onChange={this.handleFieldChange} type="text"
+            id="registerName"
+            placeholder="Display Name"
+            required="" autoFocus="" />
+          <br />
+
+          <label htmlFor="inputEmail">
+            Email address
+                </label>
+          <br />
+          <input onChange={this.handleFieldChange} type="email"
+            id="registerEmail"
+            placeholder="Email address"
+            required="" autoFocus="" />
+          <br />
+
+          <label htmlFor="inputPassword">
+            Password
+                </label>
+          <br />
+          <input onChange={this.handleFieldChange} type="password"
+            id="registerPassword"
+            placeholder="Password"
+            required="" />
+          <br />
+          <button type="submit">
+            Register
+                </button>
+        </form>
+      </React.Fragment>
+    )
+  }
 }
