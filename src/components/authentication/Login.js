f7e054168d8ccdcf70e25e1dca6357adbe3ccceb
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2018-11-29 15:35:45 -0600

    added login validation and user id is saved to local or session storage depending on remember me checkbox

--- src/components/authentication/Login.js
+++ src/components/authentication/Login.js
@@ -3,72 +3,76 @@ import React, { Component } from "react"
 
 export default class Login extends Component {
 
-    // Set initial state
-    state = {
-        email: "",
-        password: "",
-        remember: ""
-    }
+  // Set initial state
+  state = {
+    email: "",
+    password: "",
+    remember: "",
+    users: []
+  }
 
-    // Update state whenever an input field is edited
-    handleFieldChange = (evt) => {
-        const stateToChange = {}
-        stateToChange[evt.target.id] = evt.target.value
-        this.setState(stateToChange)
-    }
+  componentDidMount() {
+    const newState = {}
 
-    // Simplistic handler for login submit
-    handleLogin = (e) => {
-        e.preventDefault()
+    this.props.getAllUsers()
+      .then((users) => newState.users = users)
+      .then(() => this.setState(newState))
+  }
 
-        if (this.state.remember==="") {
-            sessionStorage.setItem(
-                "credentials",
-                JSON.stringify({
-                    email: this.state.email,
-                    password: this.state.password
-                })
-            )
-        } else {
+  // Update state whenever an input field is edited
+  handleFieldChange = (evt) => {
+    const stateToChange = {}
+    stateToChange[evt.target.id] = evt.target.value
+    this.setState(stateToChange)
+  }
+
+  // Simplistic handler for login submit
+  handleLogin = (e) => {
+    e.preventDefault()
+    this.state.users.forEach(user => {
+      if (user.email === this.state.email) {
+        if (this.state.remember === "") {
+          sessionStorage.setItem(
+            "userId", user.id
+          )} else {
             localStorage.setItem(
-                "credentials",
-                JSON.stringify({
-                    email: this.state.email,
-                    password: this.state.password
-                })
+              "userId", user.id
             )
-        }
-    }
+          }
+      }
+    })
+  }
 
-    render() {
-        return (
-            <form className="animalForm" onSubmit={this.handleLogin}>
-                <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-                <label htmlFor="inputEmail">
-                    Email address
+
+render() {
+  return (
+    <form className="bryans__class" onSubmit={this.handleLogin}>
+      <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+      <label htmlFor="inputEmail">
+        Email address
                 </label>
-                <br/>
-                <input onChange={this.handleFieldChange} type="email"
-                       id="email"
-                       placeholder="Email address"
-                       required="" autoFocus="" />
-                <br/>
-                <label htmlFor="inputPassword">
-                    Password
+      <br />
+      <input onChange={this.handleFieldChange} type="email"
+        id="email"
+        placeholder="Email address"
+        required="" autoFocus="" />
+      <br />
+      <label htmlFor="inputPassword">
+        Password
                 </label>
-                <br/>
-                <input onChange={this.handleFieldChange} type="password"
-                       id="password"
-                       placeholder="Password"
-                       required="" />
-                <br/>
-                Remember me <input onChange={this.handleFieldChange} type="checkbox"
-                       id="remember" />
-                <br/>
-                <button type="submit">
-                    Sign in
+      <br />
+      <input onChange={this.handleFieldChange} type="password"
+        id="password"
+        placeholder="Password"
+        required="" />
+      <br />
+      Remember me <input onChange={this.handleFieldChange} type="checkbox"
+        id="remember" />
+      <br />
+      <button type="submit">
+        Sign in
                 </button>
-            </form>
-        )
-    }
+    </form>
+  )
+}
 }
